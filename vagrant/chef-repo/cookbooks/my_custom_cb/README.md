9ff5dfbe7aefa7e00ac7a8513ef466b0299b02a1
Forbear <silnyagindan@gmail.com>
2017-08-24 12:17:44 +0300

    time to check

---
+++ vagrant/chef-repo/cookbooks/my_custom_cb/README.md
@@ -0,0 +1,4 @@
+# my_custom_cb
+
+TODO: Enter the cookbook description here.
+
---
+++ vagrant/chef-repo/cookbooks/task1/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: task1
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'httpd' do
+  action :install
+end
+service 'httpd' do
+  action [ :enable, :start ]
+end
